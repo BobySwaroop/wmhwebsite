eac30bd2fb57bf306a6fa8a444ce9c9d496d534c
bobyswaroop <bobyswaroop555024@gmail.com>
2023-03-27 17:21:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import { FirebaseProvider } from './Components/firebase-config';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Home from './Components/Home/Home';
+import SignIn from './Components/SignIn/SignIn';
+import SignUp from './Components/SignIn/SignUp';
+
+
+function App() {
+  return (
+    <>
+    <Router>
+      <FirebaseProvider>
+    <Routes>
+      <Route>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/admin' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+      </Route>
+    </Routes>
+    </FirebaseProvider>
+    </Router>
+    </>
+  );
+}
+
+export default App;
